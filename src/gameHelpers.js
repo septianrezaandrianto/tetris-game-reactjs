27de822603696aeb0e25d42b23a51d4e20b1c69d
Septian Reza A <septianreza.bahana@gmail.com>
2020-05-05 00:06:31 +0700

    uploaded

---
+++ src/gameHelpers.js
@@ -0,0 +1,31 @@
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
+
+export const createStage = () => 
+    Array.from(Array(STAGE_HEIGHT), () =>
+        new Array(STAGE_WIDTH).fill([0, 'clear'])
+    );
+
+export const checkCollision = (player, stage, {x : moveX, y:moveY}) => {
+    // Using for loops to be able to return (and break). Not possible with forEach
+    for (let y = 0; y < player.tetromino.length; y +=1) {
+        for (let x = 0; x < player.tetromino[y].length ; x += 1) {
+            // 1. check that we're on an actual tetromino cell
+            if ( player.tetromino[y][x] !== 0) {
+                if (
+                // 2. check that ouw move is inside game areas height (y)
+                // we shouldn't go through the bottom of the play area
+                !stage[y + player.pos.y + moveY] ||
+                // 3. check that or move is inside the game area width (x)
+                !stage[y + player.pos.y + moveY][x +player.pos.x + moveX] ||
+                // 4. check that the cell we're moving to isn't set to clear
+                stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+                ) {
+                    return true;
+                }
+            }
+        }
+    }
+    // 5. If everything above is false
+  return false;
+};    
